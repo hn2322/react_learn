2110c14eb99cbbabd4dc3842172890b973a77555
Yong <hn2322@naver.com>
2023-11-21 23:43:04 +0900

    Feat: add useEffect
    useEffect를 활용하는 구문 작성

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import Button from './Button';
+import styles from './App.module.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+    const [counter, setValue] = useState(0);
+    const [keyword, setKeyword] = useState('');
+    const onClick = () => setValue((pre) => pre + 1);
+    const onChange = (event) => {
+        setKeyword(event.target.value);
+    };
+    useEffect(() => {
+        console.log('I run only once.');
+    }, []);
+    useEffect(() => {
+        console.log("I run when 'keyword' changes");
+    }, [keyword]);
+    useEffect(() => {
+        console.log("I run when 'counter' changes");
+    }, [counter]);
+    useEffect(() => {
+        console.log("I run when 'keyword' & 'counter' changes");
+    }, [keyword, counter]);
 
-  return (
-    <div>
-      <h1 className={styles.title}>Welcome back!!!</h1>
-      <Button text={"Continue"} />
-    </div>
-  );
+    return (
+        <div>
+            <input value={keyword} onChange={onChange} type="text" placeholder="search here..." />
+            <h1>{counter}</h1>
+            <button onClick={onClick}>Click me</button>
+        </div>
+    );
 }
 
 export default App;
